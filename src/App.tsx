2a7172e744dff59162c9f3c38780538550eac696
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2025-06-22 23:25:33 -0300

    0.4 usando TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+interface cadastrado{
+  nome: string
+  idade: string
+}
+
 export default function App(){
   const [input, setInput] = useState('')
   const [idade, setIdade] = useState('')
 
+  const [info, setInfo] = useState<cadastrado>()
+
   function mostraAl(){
-    console.log(input)
+    setInfo({
+      nome: input,
+      idade: idade,
+    })
   }
 
   return(
@@ -17,12 +27,19 @@ export default function App(){
       value={input} 
       onChange={(e)=>setInput(e.target.value)}/>
 
+      <br/>
+
       <input placeholder='digite o nome ' 
       value={idade} 
       onChange={(e)=>setIdade(e.target.value)}/>
 
       <br/>
       <button onClick={mostraAl}>Mostrar aluno</button>
+
+      <hr/>
+
+      <h3>Bem vindo {info?.nome}</h3>
+      <h3>Idade: {info?.idade}</h3>
     </div>
   )
 }
